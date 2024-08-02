cf178459c3f0e9c1f061909997d1a2bc7ed84537
Mo Asif <141583278+asifkhan2513@users.noreply.github.com>
2024-08-03 00:41:29 +0400

    Diagram Setup

---
+++ BookMyShow/src/Show.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Show {
+ private  int id;
+ private Date showTime;
+ private  int availableSeat;
+  private  Movie movie;
+  private  Theater theater;
+
+}
---
+++ BookMyShow/src/Theater.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Theater {
+    private int id;
+    private String name;
+    private  String location;
+    private  int capacity;
+    private ArrayList<Show> shows;
+}
